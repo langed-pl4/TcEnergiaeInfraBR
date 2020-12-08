871c2547c9489c724c4a31d208e571d632e0a1a6
Diogo <diogo.lange@gmail.com>
2020-12-08 08:58:27 -0300

    env

---
+++ setEnv.bat
@@ -0,0 +1,21 @@
+@echo off
+@echo %0%
+:: created by Milo Boyle 
+:: used to set environment variables
+set STARTER_HOME="%CD%"
+
+
+:: //START CHANGE
+:: //Edit the following variables to match your Teamcenter Environment
+:: --------------------------------------------
+
+
+set TC_ROOT=C:\Siemens\TC12
+set TC_DATA=C:\Siemens\tc_data
+
+set TC_USER=infodba
+set TC_PASS=infodba
+set TC_GROUP=dba
+
+:: --------------------------------------------
+:: //END CHANGE
